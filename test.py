0171076f5eac041b3f888515ddaf00f4e618ff8d
yixiaohong <hongyixiao@outlook.com>
2020-01-23 15:01:26 -0500

    hhh

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import pandas as pd
-dd = 'https://raw.githubusercontent.com/YixiaoHong/MIE1624_project1/master/online_shoppers_intention.csv'
-df = pd.read_csv(dd, error_bad_lines=False)
+d = 6
+url = 'https://raw.githubusercontent.com/YixiaoHong/MIE1624_project1/master/online_shoppers_intention.csv'
+d = 7
+df = pd.read_csv(url, error_bad_lines=False)
 df.dtypes
